2c888cf05b12d98f4cbb6eed54a6aa6831b89d65
Developers@Work <DevelopersWork@users.noreply.github.com>
2021-07-08 18:45:01 +0530

    #1: AuthMe config completed

--- run.bat
+++
@@ -1,2 +0,0 @@
-python ./scripts/deploy.py
-java -Xmx4G -Xms1G -jar paper-1.17-69.jar nogui
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+# python /server/scripts/deploy.py
+# java -Xmx4G -Xms1G -jar paper-1.17-69.jar nogui
